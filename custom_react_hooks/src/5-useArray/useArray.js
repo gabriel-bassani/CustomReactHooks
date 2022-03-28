ab93e2d022317af6cfdbb54e87421ebb9b6b42e6
Bassani <gabrielbassabarreto@gmail.com>
2022-03-27 23:33:20 -0300

    adicao de funcoes push, filter e  update

--- custom_react_hooks/src/5-useArray/useArray.js
+++ custom_react_hooks/src/5-useArray/useArray.js
@@ -2,4 +2,20 @@ import { useState } from "react";
 
 export default function useArray(defaultValue) {
   const [array, setArray] = useState(defaultValue);
+
+  function push(element) {
+    setArray((a) => [...a, element]);
+  }
+
+  function filter(index, newElement) {
+    setArray((a) => a.filter(callback));
+  }
+
+  function update(index, newElement) {
+    setArray((a) => [
+      ...a.slice(0, index),
+      newElement,
+      ...a.slice(index + 1, a.length - 1),
+    ]);
+  }
 }
